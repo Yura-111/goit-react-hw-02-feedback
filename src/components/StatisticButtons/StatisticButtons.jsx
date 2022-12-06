06a3f1faca3aa78f079516ff81a0b8eb9aa775a1
Yura <104792276+Yura-111@users.noreply.github.com>
2022-12-06 22:52:18 +0200

    Add

---
+++ src/components/StatisticButtons/StatisticButtons.jsx
@@ -0,0 +1,26 @@
+// import { Component } from "react";
+// import {Statistic} from "../Statistic/Statistic";
+
+// export class StatisticButtons extends Component {
+//     state = {
+//         good: 0,
+//         neutral: 0,
+//         bad: 0
+//         }
+
+//         render()  {
+//             const {
+//                 buttons: {
+//                     good,
+//                     neutral,
+//                     bad,
+//                 }
+//             } = this.this.props;
+//         } 
+
+//         return(
+//             <Statistic>
+            
+//             <Statistic/>
+//         )
+// }
